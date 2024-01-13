e7a935e98caa9ed052e7c6c09acffbd84a5a5764
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-01-13 14:13:54 -0500

    testing async def

--- src/extraction.py
+++ src/extraction.py
@@ -2,18 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
+from utils import timer_decorator, async_timer_decorator
+import aiohttp
+import asyncio
 
 BASE_URL = 'https://listado.mercadolibre.com.co'
 
-def main(product):
+@async_timer_decorator
+async def main(product):
     list_df = []
-    initial_df, follow = organize_page_data(product=product)
-    initial_df['vehicle_info'] = initial_df['link'].apply(get_vehicle_info)
+    initial_df, follow = await organize_page_data(product=product)
+    #initial_df['vehicle_info'] = initial_df['link'].apply(get_vehicle_info)
     list_df.append(initial_df)
     while True:
         print('follow_page: ', follow)
-        follow_df, follow = organize_page_data(url=follow)
-        follow_df['vehicle_info'] = follow_df['link'].apply(get_vehicle_info)
+        follow_df, follow = await organize_page_data(url=follow)
+        #follow_df['vehicle_info'] = follow_df['link'].apply(get_vehicle_info)
         list_df.append(follow_df)
         follow_df.rename(columns={None:product}, inplace=True)
         print(follow_df.columns)
@@ -21,8 +25,8 @@ def main(product):
             break
     return pd.concat(list_df)
         
-def organize_page_data(url: str = BASE_URL ,product= None):
-    s = get_soup_by_url(url=url, product=product)
+async def organize_page_data(url: str = BASE_URL ,product= None):
+    s = await get_soup_by_url(url=url, product=product)
     products = get_all_product_names_for_page(s)
     follow = None
     try:
@@ -35,15 +39,15 @@ def organize_page_data(url: str = BASE_URL ,product= None):
     return pd.DataFrame(output_dict), follow
 
 
-def get_soup_by_url(url, product: str = None):
-    if product is None:
-        url = url
-    else:
-        url = f'{url}/{product}'
-    r = requests.get(url=url)
-    s = BeautifulSoup(r.content, 'html.parser')
-    return s
-
+async def get_soup_by_url(url, product=None):
+   if product is None:
+       url = url
+   else:
+       url = f'{url}/{product}'
+   async with aiohttp.ClientSession() as session:
+       async with session.get(url) as response:
+           s = BeautifulSoup(await response.text(), 'html.parser')
+           return s
 
 def get_all_product_names_for_page(s):
     product_names = s.find_all('h2', attrs= {"class":"ui-search-item__title"})
@@ -66,8 +70,8 @@ def get_all_product_urls_for_page(s):
     product_url = [h.get('href') for h in product_url]
     return product_url
 
-def get_vehicle_info(url):
-    s = get_soup_by_url(url)
+async def get_vehicle_info(url):
+    s = await get_soup_by_url(url)
     text = s.find_all('span', attrs= {'class':'ui-pdp-subtitle'})[0].text.replace('.', '')
     parts = text.split(' · ')
     location = s.find('p', attrs = {'class':'ui-seller-info__status-info__subtitle'}).text
@@ -88,6 +92,6 @@ def get_vehicle_info(url):
 
 
 if __name__ == '__main__':
-    data = main(product='carros')
+    data = asyncio.run(main(product='carros'))
     data.to_csv('final_data.csv')
     
