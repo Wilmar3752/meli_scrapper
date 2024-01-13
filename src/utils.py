87912cbd3b16829aec5b7bba594f9684f7000575
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-01-13 18:36:48 -0500

    add async code

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+import pandas as pd
 
 def timer_decorator(func):
    def wrapper(*args, **kwargs):
@@ -19,3 +20,11 @@ def async_timer_decorator(func):
        print(f"Function {func.__name__} took {end_time - start_time:.4f} seconds to execute")
        return result
    return wrapper
+
+def run_async(func, *args):
+   loop = asyncio.get_event_loop()
+   return loop.run_until_complete(func(*args))
+
+def async_apply(df, func, *args, **kwargs):
+   loop = asyncio.get_event_loop()
+   return pd.Series(loop.run_until_complete(asyncio.gather(*(func(x, *args, **kwargs) for x in df))))
