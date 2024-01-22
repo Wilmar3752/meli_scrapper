7de3628e3c5e360e0caf5ce3af28defdf7e2cfd7
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-01-21 19:17:01 -0500

    Feature(extraction) add number of pages to scrape

--- src/extraction.py
+++ src/extraction.py
@@ -10,21 +10,26 @@ import json
 BASE_URL = 'https://listado.mercadolibre.com.co'
 
 @async_timer_decorator
-async def main(product):
+async def main(product, pages = 1):
     list_df = []
     initial_df, follow = await organize_page_data(product=product)
     tasks = [get_vehicle_info(x) for x in initial_df['link']]
     initial_df[f'{product}_info'] = await asyncio.gather(*tasks)
     list_df.append(initial_df)
-    # while True:
-    #     print('follow_page: ', follow)
-    #     follow_df, follow = await organize_page_data(url=follow)
-    #     follow_df['vehicle_info'] = follow_df['link'].apply(get_vehicle_info)
-    #     list_df.append(follow_df)
-    #     follow_df.rename(columns={None:product}, inplace=True)
-    #     print(follow_df.columns)
-    #     if follow is None:
-    #         break
+    count=0
+    if pages >= 1:
+        while True:
+            count += 1
+            if count >= pages or follow is None:
+                break
+            print('follow_page: ', follow)
+            follow_df, follow = await organize_page_data(url=follow)
+            tasks = [get_vehicle_info(x) for x in initial_df['link']]
+            follow_df[f'{product}_info'] = await asyncio.gather(*tasks)
+            list_df.append(follow_df)
+            follow_df.rename(columns={None:product}, inplace=True)
+            print("confador de follows ",count)
+
     final_data = pd.concat(list_df)
     output = json.loads(final_data.to_json(orient='records'))
     return output
@@ -44,17 +49,25 @@ async def organize_page_data(url: str = BASE_URL ,product= None):
 
 
 async def get_soup_by_url(url, product=None):
-   proxy = generate_proxy_url()
-   print(proxy)
-   if product is None:
-       url = url
-   else:
-       url = f'{url}/{product}'
-   async with aiohttp.ClientSession() as session:
-       async with session.get(url, proxy = proxy) as response:
-           s = BeautifulSoup(await response.text(encoding='utf-8', errors='ignore'), 'html.parser')
-           return s
-
+    proxy = generate_proxy_url()
+    if product is None:
+        url = url
+    else:
+        url = f'{url}/{product}'
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(url, proxy = proxy) as response:
+                s = BeautifulSoup(await response.text(encoding='utf-8', errors='ignore'), 'html.parser')
+                return s
+        except aiohttp.client_exceptions.ClientConnectorSSLError:
+            print("Failed to establish a secure connection due to SSL error.")
+            return None
+        except aiohttp.client_exceptions.ServerDisconnectedError:
+            print("Server disconnected unexpectedly.")
+            return None
+        except aiohttp.client_exceptions.ClientHttpProxyError:
+            print("Failed to connect through proxy.")
+            return None
 def get_all_product_names_for_page(s):
     product_names = s.find_all('h2', attrs= {"class":"ui-search-item__title"})
     product_names = [v.text for v in product_names]
@@ -78,6 +91,11 @@ def get_all_product_urls_for_page(s):
 
 async def get_vehicle_info(url):
    s = await get_soup_by_url(url)
+    
+   if s is None:
+    print("Failed to retrieve webpage content.")
+    return {'eror'}
+   
    try:
        text = s.find_all('span', attrs= {'class':'ui-pdp-subtitle'})[0].text.replace('.', '')
    except IndexError:
