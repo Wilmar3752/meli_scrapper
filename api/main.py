cb53a9526b5ba85857ad1d1e76a3b8e7bc6c6b8b
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-01-21 19:17:32 -0500

    Modify imput model to API

--- api/main.py
+++ api/main.py
@@ -6,11 +6,12 @@ app = FastAPI()
 
 class Product(BaseModel):
     product: str = Field("Producto a buscar", example = "carros")
+    pages: int = Field('Paginas a buscar', example= 2)
 
 @app.post("/product")
 async def get_data(product: Product):
     print(product)
-    data = await main(product.product)
+    data = await main(product.product, product.pages)
     return data
 
 @app.get("/heart-beat")
