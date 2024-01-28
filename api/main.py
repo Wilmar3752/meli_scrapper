f7252af393d245022cac8eeefbd30a83a841516f
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-01-28 15:04:23 -0500

    Not using async

--- api/main.py
+++ api/main.py
@@ -1,16 +1,19 @@
+from typing import Union
 from fastapi import FastAPI
-from src.extraction import main
+from src.extraction_normal import main
 from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class Product(BaseModel):
     product: str = Field("Producto a buscar", example = "carros")
+    pages: Union[int, str] = Field("Number of pages to scrape", example = "all")
 
 @app.post("/product")
 async def get_data(product: Product):
     print(product)
-    data = await main(product.product)
+    data = main(product=product.product,
+                      pages= product.pages)
     return data
 
 @app.get("/heart-beat")
